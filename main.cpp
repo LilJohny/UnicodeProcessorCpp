01fec2b9940f959f635c3e5de2be27756099529d
Denis <mister.denis67808@gmail.com>
2020-04-25 23:14:16 +0300

    Implemented reading from file in bytes vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstddef>
+#include <filesystem>
+
+auto read_all_bytes_fom_file(const std::string &current_file) {
+    std::ifstream raw_file(current_file, std::ios::binary);
+    size_t length = std::filesystem::file_size(current_file);
+    std::vector<std::byte> data(length);
+    raw_file.read(reinterpret_cast<char *>(data.data()), length);
+    return data;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto file = read_all_bytes_fom_file("../total_value.txt");
+    std::cout<<(static_cast<int>(file[0]) == '\x70')<<"\n";
+
     return 0;
 }
