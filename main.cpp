67a88ce8970a913fbeee368aaf0a8e1a47f8ddd5
Denis <mister.denis67808@gmail.com>
2020-04-26 01:14:04 +0300

    Implemented work with BOM, reading file and parsing cmd args

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 #include <vector>
 #include <cstddef>
 #include <filesystem>
+#include <map>
+#include <functional>
+
+using byte = std::byte;
+
+std::pair<std::string, std::string> parse_args(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cerr << "You need to specify file  as first argument";
+        throw std::runtime_error("File is not specified");
+    } else if (argc == 2) {
+        return std::make_pair(argv[1], "");
+    }
+}
 
 auto read_all_bytes_fom_file(const std::string &current_file) {
     std::ifstream raw_file(current_file, std::ios::binary);
@@ -13,10 +26,87 @@ auto read_all_bytes_fom_file(const std::string &current_file) {
     return data;
 }
 
+std::pair<int, std::string> get_encoding(std::vector<std::byte> &bytes) {
+    std::pair<int, std::string> encoding;
+    if (bytes[0] == byte('\xef') && bytes[1] == byte('\xbb') && bytes[2] == byte('\xbf')) {
+        encoding = std::make_pair(8, "");
+        bytes = std::vector(bytes.begin() + 3, bytes.end());
+        return encoding;
+    } else if (bytes[0] == byte('\xff') && bytes[1] == byte('\xfe')) {
+        encoding = std::make_pair(16, "le");
+        bytes = std::vector(bytes.begin() + 2, bytes.end());
+        return encoding;
+    } else if (bytes[0] == byte('\xfe') && bytes[1] == byte('\xff')) {
+        encoding = std::make_pair(16, "be");
+        bytes = std::vector(bytes.begin() + 2, bytes.end());
+        return encoding;
+    } else if (bytes[0] == byte('\xff') && bytes[1] == byte('\xfe') && bytes[2] == byte('\x00') &&
+               bytes[3] == byte('\x00')) {
+        encoding = std::make_pair(16, "le");
+        bytes = std::vector(bytes.begin() + 4, bytes.end());
+        return encoding;
+    } else if (bytes[0] == byte('\x00') && bytes[1] == byte('\x00') && bytes[2] == byte('\xfe') &&
+               bytes[3] == byte('\xff')) {
+        encoding = std::make_pair(16, "be");
+        bytes = std::vector(bytes.begin() + 4, bytes.end());
+        return encoding;
+    }
+    return std::make_pair(0, "");
+}
 
-int main() {
-    auto file = read_all_bytes_fom_file("../total_value.txt");
-    std::cout<<(static_cast<int>(file[0]) == '\x70')<<"\n";
+size_t count_code_points(std::vector<std::vector<byte>> bytes, std::pair<std::string, std::string> encoding) {
+    return 0;
+}
 
+size_t count_code_units(std::vector<std::vector<byte>> bytes, std::pair<std::string, std::string> encoding) {
+    return 0;
+}
+
+size_t count_words(std::vector<std::vector<byte>> bytes, std::pair<std::string, std::string> encoding) {
+    return 0;
+}
+
+auto normalize_utf8(const std::vector<byte> &bytes) {
+    std::vector<std::vector<byte>> normalized_bytes;
+    return normalized_bytes;
+}
+
+auto normalize_utf16(const std::vector<byte> &bytes) {
+    std::vector<std::vector<byte>> normalized_bytes;
+//    for (int i = 0; i < bytes.size();) {
+//        if()
+//    }
+    return normalized_bytes;
+}
+
+auto normalize_utf32(const std::vector<byte> &bytes) {
+    std::vector<std::vector<byte>> normalized_bytes;
+    for (int i = 0; i < bytes.size(); i += 4) {
+        normalized_bytes.emplace_back(bytes.begin() + i, bytes.begin() + i + 3);
+    }
+    return normalized_bytes;
+}
+
+auto normalize_according_to_encoding(const std::vector<byte> &bytes, int encoding) {
+    std::vector<std::vector<byte>> normalized_bytes;
+    std::map<int, std::function<std::vector<std::vector<byte>>(std::vector<byte>)>> normalizers
+            = {{8,  std::function(normalize_utf8)},
+               {16, std::function(normalize_utf16)},
+               {32, std::function(normalize_utf32)}};
+    auto normalizer = normalizers[encoding];
+    normalized_bytes = normalizer(bytes);
+    return normalized_bytes;
+}
+
+int main(int argc, char *argv[]) {
+    auto args = parse_args(argc, argv);
+    auto input_file = args.first;
+    auto encoding = args.second;
+    auto file_bytes = read_all_bytes_fom_file(input_file);
+
+    auto deducted_encoding = get_encoding(file_bytes);
+    if (deducted_encoding.first == 0) {
+        throw std::runtime_error("Dont supported file encoding");
+    }
     return 0;
 }
