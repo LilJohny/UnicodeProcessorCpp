2aa615de3ce486088c45042d8d8d405e6a573c9f
Denis <mister.denis67808@gmail.com>
2020-04-27 01:08:12 +0300

    Implemented surrogates checking

--- utf16.cpp
+++ utf16.cpp
@@ -1,11 +1,24 @@
 //
 // Created by denis on 26.04.20.
 //
+#include <cstddef>
 #include "utf16.h"
-bool utf16::is_valid(std::vector<std::byte> bytes) {
-    return 16;
+
+bool utf16::is_high_surrogate(std::byte byte) {
+    auto byte_short = std::to_integer<unsigned short>(byte);
+    return byte_short >= HIGH_SURROGATE_MIN && byte_short <= HIGH_SURROGATE_MAX;
 }
-std::vector<std::vector<std::byte> > utf16::normalize(const std::vector<std::byte>& bytes) {
+
+bool utf16::is_low_surrogate(std::byte byte) {
+    auto byte_short = std::to_integer<unsigned short>(byte);
+    return byte_short >= LOW_SURROGATE_MIN && byte_short <= LOW_SURROGATE_MAX;
+}
+
+bool utf16::is_valid(std::vector<std::byte> bytes) {
+    return false;
+}
+
+std::vector<std::vector<std::byte> > utf16::normalize(const std::vector<std::byte> &bytes) {
     std::vector<std::vector<std::byte>> normalized_bytes;
     return normalized_bytes;
 }
--- utf16.h
+++ utf16.h
@@ -12,8 +12,14 @@ namespace utf16 {
     const std::byte BOM_BE_SECOND = std::byte('\xff');
     const std::byte BOM_LE_FIRST = std::byte('\xff');
     const std::byte BOM_LE_SECOND = std::byte('\xfe');
+    const unsigned short HIGH_SURROGATE_MIN = 0xd800;
+    const unsigned short HIGH_SURROGATE_MAX = 0xdbff;
+    const unsigned short LOW_SURROGATE_MIN = 0xdc00;
+    const unsigned short LOW_SURROGATE_MAX = 0xdfff;
 
     bool is_valid(std::vector<std::byte> bytes);
-    std::vector<std::vector<std::byte>> normalize(const std::vector<std::byte>& bytes);
+    bool is_high_surrogate(std::byte byte);
+    bool is_low_surrogate(std::byte byte);
+    std::vector<std::vector<std::byte>> normalize(const std::vector<std::byte> &bytes);
 }
 #endif //UNICODEPROCESSOR_UTF16_H
