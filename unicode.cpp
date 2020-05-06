952730eb9718f672ff75ab0b75ef75db87b53896
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-07 00:03:17 +0300

    Fixed encoding order

--- unicode.cpp
+++ unicode.cpp
@@ -18,27 +18,27 @@ std::vector<std::byte> unicode::read_all_bytes_fom_file(const std::string &curre
 std::pair<int, std::string> unicode::get_encoding(std::vector<std::byte> &bytes) {
   std::pair<int, std::string> encoding;
   if (bytes[0] == utf8::BOM_FIRST && bytes[1] == utf8::BOM_SECOND && bytes[2] == utf8::BOM_THIRD) {
-    encoding = std::make_pair(8, "bom");
-    bytes = std::vector(bytes.begin() + 3, bytes.end());
-    return encoding;
-  } else if (bytes[0] == utf16::BOM_LE_FIRST && bytes[1] == utf16::BOM_LE_SECOND) {
-    encoding = std::make_pair(16, "le");
-    bytes = std::vector(bytes.begin() + 2, bytes.end());
-    return encoding;
-  } else if (bytes[0] == utf16::BOM_BE_FIRST && bytes[1] == utf16::BOM_BE_SECOND) {
-    encoding = std::make_pair(16, "be");
-    bytes = std::vector(bytes.begin() + 2, bytes.end());
-    return encoding;
+	encoding = std::make_pair(8, "bom");
+	bytes = std::vector(bytes.begin() + 3, bytes.end());
+	return encoding;
   } else if (bytes[0] == utf32::BOM_LE_FIRST && bytes[1] == utf32::BOM_LE_SECOND && bytes[2] == unicode::NULL_BYTE &&
-      bytes[3] == unicode::NULL_BYTE) {
-    encoding = std::make_pair(32, "le");
-    bytes = std::vector(bytes.begin() + 4, bytes.end());
-    return encoding;
+	  bytes[3] == unicode::NULL_BYTE) {
+	encoding = std::make_pair(32, "le");
+	bytes = std::vector(bytes.begin() + 4, bytes.end());
+	return encoding;
+  } else if (bytes[0] == utf16::BOM_LE_FIRST && bytes[1] == utf16::BOM_LE_SECOND) {
+	encoding = std::make_pair(16, "le");
+	bytes = std::vector(bytes.begin() + 2, bytes.end());
+	return encoding;
   } else if (bytes[0] == unicode::NULL_BYTE && bytes[1] == unicode::NULL_BYTE && bytes[2] == utf32::BOM_BE_THIRD &&
-      bytes[3] == utf32::BOM_BE_FOURTH) {
-    encoding = std::make_pair(32, "be");
-    bytes = std::vector(bytes.begin() + 4, bytes.end());
-    return encoding;
+	  bytes[3] == utf32::BOM_BE_FOURTH) {
+	encoding = std::make_pair(32, "be");
+	bytes = std::vector(bytes.begin() + 4, bytes.end());
+	return encoding;
+  } else if (bytes[0] == utf16::BOM_BE_FIRST && bytes[1] == utf16::BOM_BE_SECOND) {
+	encoding = std::make_pair(16, "be");
+	bytes = std::vector(bytes.begin() + 2, bytes.end());
+	return encoding;
   }
   return std::make_pair(0, "");
 }
@@ -52,12 +52,12 @@ size_t unicode::count_code_units(const std::vector<std::byte> &bytes) {
 }
 
 size_t unicode::count_words(const std::vector<std::vector<std::byte> > &bytes,
-                            const std::pair<int, std::string> &encoding) {
-  int order = encoding.second == "be" ? -1 : 1;
+							const std::pair<int, std::string> &encoding) {
+  int order = encoding.second == "be" ? 1 : -1;
   std::map<int, std::function<size_t(const std::vector<std::vector<std::byte>> &, int)>> counters
-      = {{8, std::function(utf8::count_words)},
-         {16, std::function(utf16::count_words)},
-         {32, std::function(utf32::count_words)}};
+	  = {{8, std::function(utf8::count_words)},
+		 {16, std::function(utf16::count_words)},
+		 {32, std::function(utf32::count_words)}};
   auto counter = counters[encoding.first];
   auto result = counter(bytes, order);
   return result;
@@ -67,9 +67,9 @@ std::vector<std::vector<std::byte>>
 unicode::normalize_according_to_encoding(const std::vector<std::byte> &bytes, int encoding) {
   std::vector<std::vector<std::byte>> normalized_bytes;
   std::map<int, std::function<std::vector<std::vector<std::byte>>(std::vector<std::byte>)>> normalizers
-      = {{8, std::function(utf8::normalize)},
-         {16, std::function(utf16::normalize)},
-         {32, std::function(utf32::normalize)}};
+	  = {{8, std::function(utf8::normalize)},
+		 {16, std::function(utf16::normalize)},
+		 {32, std::function(utf32::normalize)}};
   auto normalizer = normalizers[encoding];
   normalized_bytes = normalizer(bytes);
   return normalized_bytes;
@@ -78,9 +78,9 @@ unicode::normalize_according_to_encoding(const std::vector<std::byte> &bytes, in
 std::vector<std::byte> unicode::validate(const std::vector<std::byte> &bytes, int encoding) {
   std::vector<std::vector<std::byte>> normalized_bytes;
   std::map<int, std::function<std::vector<std::byte>(const std::vector<std::byte> &)>> validators
-      = {{8, std::function(utf8::validate)},
-         {16, std::function(utf16::validate)},
-         {32, std::function(utf32::validate)}};
+	  = {{8, std::function(utf8::validate)},
+		 {16, std::function(utf16::validate)},
+		 {32, std::function(utf32::validate)}};
   auto validator = validators[encoding];
   auto bad_bits = validator(bytes);
   return bad_bits;
