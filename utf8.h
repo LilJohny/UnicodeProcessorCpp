f3ef0ad11b152efa1581fdcfec93c27bafcf4cd2
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-26 01:52:00 +0300

    Refactoring

--- utf8.h
+++ utf8.h
@@ -25,37 +25,37 @@ const int SINGLE_UNIT_MAX = 127;
 const int SINGLE_UNIT_MIN = 0;
 
 const std::vector<std::byte> WHITESPACES_S = {std::byte('\x09'), std::byte('\x0a'), std::byte('\x0b'),
-											  std::byte('\x0c'), std::byte('\x0d'), std::byte('\x20')};
+																							std::byte('\x0c'), std::byte('\x0d'), std::byte('\x20')};
 const std::vector<std::vector<std::byte>> WHITESPACES_D = {
-	{std::byte('\xc2'), std::byte('\x85')},
-	{std::byte('\xc2'), std::byte('\xa0')}
+		{std::byte('\xc2'), std::byte('\x85')},
+		{std::byte('\xc2'), std::byte('\xa0')}
 };
 const std::vector<std::vector<std::byte>> WHITESPACES_T = {
-	{std::byte('\xe1'), std::byte('\x9a'), std::byte('\x80')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x81')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x82')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x83')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x84')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x85')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x86')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x87')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x88')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x89')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8a')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\xa8')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\xa9')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\xaf')},
-	{std::byte('\xe2'), std::byte('\x81'), std::byte('\x9f')},
-	{std::byte('\xe3'), std::byte('\x80'), std::byte('\x80')},
-	{std::byte('\xe1'), std::byte('\xa0'), std::byte('\x8e')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8b')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8c')},
-	{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8d')},
-	{std::byte('\xe2'), std::byte('\x81'), std::byte('\xa0')},
-	{std::byte('\xef'), std::byte('\xbb'), std::byte('\xbf')}
+		{std::byte('\xe1'), std::byte('\x9a'), std::byte('\x80')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x81')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x82')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x83')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x84')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x85')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x86')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x87')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x88')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x89')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8a')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\xa8')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\xa9')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\xaf')},
+		{std::byte('\xe2'), std::byte('\x81'), std::byte('\x9f')},
+		{std::byte('\xe3'), std::byte('\x80'), std::byte('\x80')},
+		{std::byte('\xe1'), std::byte('\xa0'), std::byte('\x8e')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8b')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8c')},
+		{std::byte('\xe2'), std::byte('\x80'), std::byte('\x8d')},
+		{std::byte('\xe2'), std::byte('\x81'), std::byte('\xa0')},
+		{std::byte('\xef'), std::byte('\xbb'), std::byte('\xbf')}
 };
 
-std::vector<int> validate_batch(const std::vector<std::byte>&batch);
+std::vector<int> validate_batch(const std::vector<std::byte> &batch);
 
 inline bool is_valid_start(std::byte byte);
 
